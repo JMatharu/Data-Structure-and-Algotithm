bae12154618f01310941e18abee67abeb50e15f6
Jagdeep Matharu <jagdeepmatharu88@gmail.com>
2019-08-29 10:42:02 -0400

    Queue linked list chart

--- Queues.playground/Sources/QueueLinkedList.swift
+++ Queues.playground/Sources/QueueLinkedList.swift
@@ -35,3 +35,21 @@ extension QueueLinkedList: CustomStringConvertible {
         return list.description
     }
 }
+
+/*
+ |-------------------------------------------------------------------|
+ |      Operation   |        Best Case       |       Worst Case      |
+ |-------------------------------------------------------------------|
+ | enqueue          |          O(1)          |          O(1)         |
+ |-------------------------------------------------------------------|
+ | dequeue          |          O(1)          |          O(1)         |
+ |-------------------------------------------------------------------|
+ | Space Complexity |          O(n)          |          O(n)         |
+ |-------------------------------------------------------------------|
+ 
+ Note: Despite O(1) performance, it suffers from high overhead. Each
+ element has to have extra storage for the forward and back reference.
+ Moreover, every time you create a new element, it requires a
+ relatively expensive dynamic allocation. By contrast QueueArray does
+ bulk allocation which is faster.
+ */
