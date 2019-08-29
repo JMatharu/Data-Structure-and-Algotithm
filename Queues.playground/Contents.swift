38b0fd7c0dde064ee94776acab31d430b3ef260e
Jagdeep Matharu <jagdeepmatharu88@gmail.com>
2019-08-29 10:30:26 -0400

    Queue linked list example

--- Queues.playground/Contents.swift
+++ Queues.playground/Contents.swift
@@ -14,8 +14,7 @@ queueArray
 queueArray.peek
 queueArray.isEmpty
 
-/*
---Result--
+/* --Result--
  true
  true
  true
@@ -23,5 +22,31 @@ queueArray.isEmpty
  Ray
  ["Brian", "Eric"]
  Brian
+ false
+ */
+
+
+/*
+ Linked List Queue
+ */
+
+var queueLinkedList = QueueLinkedList<String>()
+queueLinkedList.isEmpty
+queueLinkedList.enqueue("Ray")
+queueLinkedList.enqueue("Brian")
+queueLinkedList.enqueue("Eric")
+queueLinkedList.dequeue()
+queueLinkedList
+queueLinkedList.peek
+queueLinkedList.isEmpty
+
+/* --Result--
+ true
+ true
+ true
+ true
+ Optional("Ray")
+ Brian -> Eric -> end
+ Optional("Brian")
  false
  */
