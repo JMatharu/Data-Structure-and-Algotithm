240af17e46281d9f8e06390c14b354bbbafbe2ea
Jagdeep Matharu <jagdeepmatharu88@gmail.com>
2020-04-06 09:23:21 -0400

    Updating peak finder

--- PeakFinder.swift
+++ PeakFinder.swift
@@ -19,16 +19,21 @@
 
 func findPeakIndexWithStraighyForward(for nums: [Int]) -> Int? {
     for i in 0..<nums.count {
-        if (nums[i] == nums.count - 1) || (nums[i] > nums[i+1]) {
-            return i
+        if i + 1 == nums.count {
+            return nums[i]
+        }
+        if nums[i] > nums[i+1] {
+            return nums[i]
         }
     }
     return nil
 }
 
-findPeakIndexWithStraighyForward(for: [4,3,2,1]) // 0
+findPeakIndexWithStraighyForward(for: [1,4,3,2,1]) // 0
 findPeakIndexWithStraighyForward(for: [0,2,4,6,7]) // 2
 findPeakIndexWithStraighyForward(for: [1,2,3,4,3,2,1]) // 3
+findPeakIndexWithStraighyForward(for: [8,6,4,3,1]) // 3
+
 /*
  "Divide & Conquer" (a recursive) algorithm
  */
