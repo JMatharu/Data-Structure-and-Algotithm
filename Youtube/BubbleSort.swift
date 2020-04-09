b783bb911ab162753a3d21c2b11475fbdb5163f3
Jagdeep Matharu <jagdeepmatharu88@gmail.com>
2020-04-09 17:57:51 -0400

    Update with results

--- Youtube/BubbleSort.swift
+++ Youtube/BubbleSort.swift
@@ -1,6 +1,42 @@
 /*
-Bubble Sort - https://www.youtube.com/watch?v=o4bAoo_gFBU&list=PLdo5W4Nhv31bEiyP4tclZMc5mP_X7OD7k&index=1
-*/
+ Two-dimensional Version
+ */
+
+/*
+ Greedy Ascent Algorithm
+ Θ(nm) complexity, Θ(n2(square)) algorithm if m = n
+ (Palle nai pya, na jada damag lagaya :P)
+ */
+
+/*
+ "2D recursive" algorithm
+ If T(n, m) denotes work required to solve problem with n rows and m columns = Θ(n log n)
+ */
+
+//func find2DPeak<T: Comparable>(nums: [[T]]) {
+//
+//    let rows = nums.count
+//    let midColumn =
+//
+//}
+//
+//let problemMatrix = [
+//    [ 4,  5,  6,  7,  8,  7,  6,  5,  4,  3,  2],
+//    [ 5,  6,  7,  8,  9,  8,  7,  6,  5,  4,  3],
+//    [ 6,  7,  8,  9, 10,  9,  8,  7,  6,  5,  4],
+//    [ 7,  8,  9, 10, 11, 10,  9,  8,  7,  6,  5],
+//    [ 8,  9, 10, 11, 12, 11, 10,  9,  8,  7,  6],
+//    [ 7,  8,  9, 10, 11, 10,  9,  8,  7,  6,  5],
+//    [ 6,  7,  8,  9, 10,  9,  8,  7,  6,  5,  4],
+//    [ 5,  6,  7,  8,  9,  8,  7,  6,  5,  4,  3],
+//    [ 4,  5,  6,  7,  8,  7,  6,  5,  4,  3,  2],
+//    [ 3,  4,  5,  6,  7,  6,  5,  4,  3,  2,  1],
+//    [ 2,  3,  4,  5,  6,  5,  4,  3,  2,  1,  0]
+//]
+//
+//find2DPeak(nums: problemMatrix)
+
+
 
 func bubbleSort_iterative<T: Comparable>(on array: inout [T]) -> [T?] {
     
@@ -30,10 +66,10 @@ var array = [10,9,7,5,6, 8]
 var array2 = [5]
 var array3 = [6,4,3,2,1]
 var array4 = [9,8,7,6,5,4,3,2,1,12,3,4,54,6,356,45,67,56,78568,756,34,56,2452]
-bubbleSort_iterative(on: &array)
-bubbleSort_iterative(on: &array2)
-bubbleSort_iterative(on: &array3)
-bubbleSort_iterative(on: &array4)
+bubbleSort_iterative(on: &array) //[10, 9, 8, 7, 6, 5]
+bubbleSort_iterative(on: &array2) //[5]
+bubbleSort_iterative(on: &array3) //[6, 4, 3, 2, 1]
+bubbleSort_iterative(on: &array4) //[78568, 2452, 756, 356, 67, 56, 56, 54, 45, 34, 12, 9, 8, 7, 6, 6, 5, 4, 4, 3, 3, 2, 1]
 
 
 
@@ -56,7 +92,7 @@ func bubbleSort_recursion<T: Comparable>(on array: inout[T]) -> [T] {
     return array
 }
 
-bubbleSort_recursion(on: &array)
-bubbleSort_recursion(on: &array2)
-bubbleSort_recursion(on: &array3)
-bubbleSort_recursion(on: &array4)
+bubbleSort_recursion(on: &array) //[10, 9, 8, 7, 6, 5]
+bubbleSort_recursion(on: &array2) //[5]
+bubbleSort_recursion(on: &array3) //[6, 4, 3, 2, 1]
+bubbleSort_recursion(on: &array4) //[78568, 2452, 756, 356, 67, 56, 56, 54, 45, 34, 12, 9, 8, 7, 6, 6, 5, 4, 4, 3, 3, 2, 1]
