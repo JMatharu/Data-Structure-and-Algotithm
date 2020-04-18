f12ecb3415c6077276117fae1c14ad130a2528a4
Jagdeep Matharu <jagdeepmatharu88@gmail.com>
2020-04-17 23:33:36 -0400

    Use generics

--- Youtube/LinkedList.swift
+++ Youtube/LinkedList.swift
@@ -2,10 +2,11 @@
     Singly Linked List
  */
 
-public class ListNode: CustomStringConvertible {
-    public var val: Int
+public class ListNode<T>: CustomStringConvertible {
+    
+    public var val: T
     public var next: ListNode?
-    public init(_ val: Int) {
+    public init(_ val: T) {
         self.val = val
         self.next = nil
     }
@@ -19,9 +20,9 @@ public class ListNode: CustomStringConvertible {
     
 }
 
-public struct LinkedList {
-    public var head: ListNode?
-    private var temp: ListNode? // Or call it tail
+public struct LinkedList<T> {
+    public var head: ListNode<T>?
+    private var temp: ListNode<T>? // Or call it tail
     
     public init() {}
     
@@ -29,31 +30,31 @@ public struct LinkedList {
         return head == nil
     }
     
-    mutating func push(_ val: Int) {
+    mutating func push(_ val: T) {
         if head == nil {
-            head = ListNode(val)
+            head = ListNode<T>(val)
             temp = head
         } else {
-            temp?.next = ListNode(val)
+            temp?.next = ListNode<T>(val)
             temp = temp?.next
         }
     }
 }
 
-let node1 = ListNode(1)
-let node2 = ListNode(2)
-let node3 = ListNode(3)
+let node1 = ListNode<Int>(1)
+let node2 = ListNode<Int>(2)
+let node3 = ListNode<Int>(3)
 
 node1.next = node2
 node2.next = node3
 
 print(node1) // 1 -> 2 -> 3
 
-var list = LinkedList()
-list.push(5)
-list.push(10)
-list.push(15)
+var list = LinkedList<String>()
+list.push("one")
+list.push("two")
+list.push("three")
 
-print(list) //LinkedList(head: Optional(5 -> 10 -> 15), temp: Optional(15))
+print(list) //LinkedList<String>(head: Optional(one -> two -> three), temp: Optional(three))
 
 
