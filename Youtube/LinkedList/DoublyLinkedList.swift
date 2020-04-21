43c3a0a27f8a3b59aa792508b781575730fc32ba
Jagdeep Matharu <jagdeepmatharu88@gmail.com>
2020-04-20 22:23:03 -0400

    Update DoublyLinkedList.swift

--- Youtube/LinkedList/DoublyLinkedList.swift
+++ Youtube/LinkedList/DoublyLinkedList.swift
@@ -59,7 +59,7 @@ extension LinkedList {
         tail = newNode
     }
     
-    // Add to first
+    // Add to first O(1)
     mutating func push(_ value: T) {
         let newNode = ListNode(value)
         guard !isEmpty else {
@@ -73,6 +73,7 @@ extension LinkedList {
         head = newNode
     }
     
+    // O(n)
     mutating func insert(_ value: T, at index: Int) {
         let newNode = ListNode(value)
         guard !isEmpty else {
@@ -81,6 +82,8 @@ extension LinkedList {
             tail = newNode
             return
         }
+        
+        guard !(index < 0) else { print("Unable to insert at index \(index)"); return }
         
         if length == index {
             append(value)
@@ -97,6 +100,60 @@ extension LinkedList {
     }
 }
 
+// Delettion
+extension LinkedList {
+    // Delete from begining O(1)
+    mutating func pop() {
+        guard !isEmpty else {
+            print("Noting to remove")
+            return
+        }
+        if length == 1 { head = nil; tail = nil; return }
+        head = head?.next
+        head?.previous = nil
+    }
+    
+    mutating func deleteFromEnd() {
+        guard !isEmpty else {
+            print("Noting to remove")
+            return
+        }
+        if length == 1 { head = nil; tail = nil; return }
+        tail = tail?.previous
+        tail?.next = nil
+    }
+    
+    // O(n)
+    mutating func delete(at index: Int) {
+        guard !isEmpty else {
+            if index > 0 || index < 0 { print("Unable to delete at index \(index)"); return }
+            head = nil
+            tail = nil
+            return
+        }
+        guard !(index < 0) else { print("Unable to delete at index \(index)"); return }
+        
+        var temp = head
+        var counter = 0
+        while counter < index - 1 {
+            temp = temp?.next
+            counter += 1
+        }
+        let nextNode = temp?.next
+        let previousNode = temp?.previous
+        previousNode?.next = nextNode
+        nextNode?.previous = previousNode
+        temp = nil
+    }
+}
+
+// Reverse
+extension LinkedList {
+    func reverse() {
+        
+    }
+}
+
 var list = LinkedList<String>()
 list.push("e")
 list.push("f")
@@ -110,5 +167,11 @@ list.push("2")
 list.head //2 <-> f <-> e <-> a <-> b <-> c <-> d
 list.insert("1", at: 3)
 list.head //2 <-> f <-> e <-> 1 <-> a <-> b <-> c <-> d
-
-
+list.pop()
+list.head //f <-> e <-> 1 <-> a <-> b <-> c <-> d
+list.deleteFromEnd()
+list.head //f <-> e <-> 1 <-> a <-> b <-> c
+list.delete(at: 2)
+list.head //f <-> 1 <-> a <-> b <-> c
+list.delete(at: 3) // Wrong here
+list.head
